d42fad05318a219ecb677eb7a497199578847341
ACatThatPrograms <25625537+ACatThatPrograms@users.noreply.github.com>
2022-04-01 10:15:21 -0400

    Fix edit links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,12 +24,12 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/MadBase/alice-docs-hub',
+          editUrl: 'https://github.com/MadBase/alice-docs-hub/tree/main',
         },
         blog: {
           showReadingTime: true,
           editUrl:
-            'https://github.com/MadBase/alice-docs-hub',
+            'https://github.com/MadBase/alice-docs-hub/tree/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
